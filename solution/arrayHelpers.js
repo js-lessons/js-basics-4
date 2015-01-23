559ce4b403044704b7576975d4b16423ef5c3675
Anastasiya Yevstyunicheva <ae@anahoret.com>
2015-01-23 16:53:07 +0200

    Wrong solution.

--- solution/arrayHelpers.js
+++ solution/arrayHelpers.js
@@ -2,17 +2,44 @@ function reduce(array, f, start) {
   // The reduce function applies a function against an accumulator
   // and each value of the array (from left-to-right) has to reduce it to a single value.
   // You can implement map, filter, flatmap, every and some using reduce
+  if (array.length) {
+    var a = array.pop();
+    start = start || 1;
+    return reduce(array, f, f(a, start))
+  } else {
+    return start
+  }
 }
 
 function map(array, f) {
   // The map function transforms an array by applying a function to all of its elements
   // and building a new array from the returned values. The new array will have
   // the same length as the input array, but its content will have been "mapped" to a new form by the function.
+  var new_array = new Array();
+  if (array.length) {
+    array.forEach(function(el) {
+      new_array.push(f(el))
+    })
+    return new_array
+  } else {
+    return array
+  }
 }
 
 function filter(array, f) {
   // The filter function creates a new array with all elements
   // that pass the test implemented by the provided function.
+  var new_array = new Array();
+  if (array.length) {
+    array.forEach(function(el) {
+      if (f(el)) {
+        new_array.push(f(el))
+      }
+    })
+    return new_array
+  } else {
+    return array
+  }
 }
 
 function flatmap(array, f) {
