ac5f0b129b16d3aedfc05eb2577ae8968ea8b598
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2015-01-27 14:33:37 +0300

    arrayHelpers done.

--- solution/arrayHelpers.js
+++ solution/arrayHelpers.js
@@ -1,33 +1,41 @@
 function reduce(array, f, start) {
-  // The reduce function applies a function against an accumulator
-  // and each value of the array (from left-to-right) has to reduce it to a single value.
-  // Here start is initial value of an accumulator. If it wasn't set then first element of the array will
-  // be used as initial value, but the array should be iterated from the second element.
-  // You can implement map, filter, flatmap, every and some using reduce
+  if (!array.length) return start;
+  if (typeof start === 'undefined') return reduce(array.slice(1, array.length), f, array[0]);
+
+  var res = start;
+  array.forEach( function (item) {
+    res = f(res, item);
+  });
+  return res;
 }
 
 function map(array, f) {
-  // The map function transforms an array by applying a function to all of its elements
-  // and building a new array from the returned values. The new array will have
-  // the same length as the input array, but its content will have been "mapped" to a new form by the function.
+  return reduce(array, function (acc, item) {
+    return acc.concat([f(item)]);
+  }, []);
 }
 
 function filter(array, f) {
-  // The filter function creates a new array with all elements
-  // that pass the test implemented by the provided function.
+  return reduce(array, function (acc, item) {
+    return f(item) ? acc.concat([item]) : acc;
+  }, []);
 }
 
 function flatmap(array, f) {
-  // Use the reduce method in combination with the concat method to "flatten"
-  // an array of arrays into a single array that has all the elements of the input arrays.
+  return reduce(array, function (acc, item) {
+    return acc.concat(f(item));
+  }, []);
 }
 
 function every(array, f) {
-  // The every function tests whether all elements in the array pass the test implemented by the provided function.
+  for (var i = 0; i < array.length; ++i) {
+    if (!f(array[i])) return false;
+  }
+  return true;
 }
 
 function some(array, f) {
-  // The some function tests whether some element in the array passes the test implemented by the provided function.
+  return !every(array, function (item) { return !f(item); });
 }
 
 module.exports = {
