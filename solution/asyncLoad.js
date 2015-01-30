f23f620ea03ab4b676359eb4416d2d58f965df67
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-01-30 15:23:49 +0300

    Added task 'Spy'

--- solution/asyncLoad.js
+++ solution/asyncLoad.js
@@ -10,16 +10,4 @@ function asyncLoad(ids, load, done) {
   });
 }
 
-// function asyncLoad2(id1, id2, load, done) {
-//   var result = [undefined, undefined];
-//   load(id1, function (resource) {
-//     result[0] = resource;
-//     if (result[1] !== undefined) done(result);
-//   });
-//   load(id2, function (resource) {
-//     result[1] = resource;
-//     if (result[0] !== undefined) done(result);
-//   });
-// }
-
 module.exports = asyncLoad;
