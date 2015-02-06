36e71010023345c3dc16d0d7a7ea2b02f1d8fb74
lush81_Shapovalenko <lush@i.ua>
2015-02-06 22:28:40 +0200

    Solutions to Exercise_4_1_ arrayHelpers from Shapovalenko

--- solution/arrayHelpers.js
+++ solution/arrayHelpers.js
@@ -4,31 +4,60 @@ function reduce(array, f, start) {
   // Here start is initial value of an accumulator. If it wasn't set then first element of the array will
   // be used as initial value, but the array should be iterated from the second element.
   // You can implement map, filter, flatmap, every and some using reduce
+if(array.length===0 && typeof start!= 'undefined'){
+array.push(1);
+}
+if( array.length>0 && typeof start=== 'undefined'){
+start=1;
+}
+return array.reduce(f,start);
 }
 
 function map(array, f) {
   // The map function transforms an array by applying a function to all of its elements
   // and building a new array from the returned values. The new array will have
   // the same length as the input array, but its content will have been "mapped" to a new form by the function.
-}
+  return array.map(f);
+  }
 
 function filter(array, f) {
   // The filter function creates a new array with all elements
   // that pass the test implemented by the provided function.
-}
+ return array.filter(f);
+  }
 
 function flatmap(array, f) {
   // Use the reduce method in combination with the concat method to "flatten"
   // an array of arrays into a single array that has all the elements of the input arrays.
-}
+var allArrays = array.map(f);
+ 
+ return allArrays.reduce(function(firstArr, secondArr) {
+  return firstArr.concat(secondArr);
+}); 
+  }
 
 function every(array, f) {
   // The every function tests whether all elements in the array pass the test implemented by the provided function.
+for( var i=0; i<array.length; i++){	
+    if(!f(array[i])){
+      return false;
+    }
+ }
+return true;
 }
+
 
 function some(array, f) {
   // The some function tests whether some element in the array passes the test implemented by the provided function.
+  for( var i=0; i<array.length; i++){	
+    if(f(array[i])){
+      return true;
+    }
+	
 }
+return false;
+}
+
 
 module.exports = {
   map: map,
